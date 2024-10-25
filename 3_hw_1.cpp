c74dcc576fc412b87dd20b60246972ce1d7cc34d
archi <162299395+lulmealearn@users.noreply.github.com>
2024-10-25 22:07:58 +0300

    Update 3_hw_1.cpp

--- 3_hw_1.cpp
+++ 3_hw_1.cpp
@@ -1,22 +1,26 @@
+// Включаем стандартный заголовочный файл для ввода/вывода
 #include <iostream>
-#include <string>
 
+// Определение структуры Author для хранения имени и фамилии автора
 struct Author {
     std::string firstName;
     std::string lastName;
 };
 
+// Определение структуры Book для хранения информации о книге
 struct Book {
-    std::string title;
-    Author author;
-    int yearOfPublication;
-    int numberOfPages;
+    std::string title;               // Название книги
+    Author author;                    // Автор книги
+    int yearOfPublication;           // Год издания
+    int numberOfPages;                // Количество страниц
 
+    // Конструктор по умолчанию для инициализации полей структуры Book
     Book() : title("Неизвестно"), yearOfPublication(0), numberOfPages(0) {
         author.firstName = "Неизвестно";
         author.lastName = "Неизвестно";
     }
 
+    // Метод для вывода информации о книге на экран
     void displayBook() const {
         std::cout << "Название книги: " << title << std::endl;
         std::cout << "Автор: " << author.firstName << " " << author.lastName << std::endl;
@@ -26,22 +30,28 @@ struct Book {
     }
 };
 
+// Функция для создания и заполнения структуры Book
 Book createBook() {
     Book book;
 
+    // Ввод названия книги
     std::cout << "Введите название книги: ";
-    std::cin.ignore();
+    std::cin.ignore();                // Игнорируем символ перевода строки в потоке ввода
     std::getline(std::cin, book.title);
 
+    // Ввод имени автора
     std::cout << "Введите имя автора: ";
     std::getline(std::cin, book.author.firstName);
 
+    // Ввод фамилии автора
     std::cout << "Введите фамилию автора: ";
     std::getline(std::cin, book.author.lastName);
 
+    // Ввод года издания
     std::cout << "Введите год издания: ";
     std::cin >> book.yearOfPublication;
 
+    // Ввод количества страниц
     std::cout << "Введите количество страниц: ";
     std::cin >> book.numberOfPages;
 
@@ -49,22 +59,28 @@ Book createBook() {
 }
 
 int main() {
-    const int numberOfBooks = 3;
+    const int numberOfBooks = 3;      // Количество книг, которые будут введены
+
+    // Массив книг
     Book books[numberOfBooks];
 
+    // Цикл для ввода данных о каждой книге
     for (int i = 0; i < numberOfBooks; ++i) {
+        // Спрашиваем пользователя, хочет ли он ввести данные для текущей книги
         std::cout << "Хотите ввести данные для книги " << i + 1 << "? (y/n): ";
         char answer;
         std::cin >> answer;
 
         if (answer == 'y' || answer == 'Y') {
+            // Если пользователь согласен, вызываем функцию createBook для заполнения структуры Book
             books[i] = createBook();
         } else {
-
+            // Если пользователь отказался, используем конструктор по умолчанию для инициализации полей структуры Book
             books[i] = Book();
         }
     }
 
+    // Цикл для вывода информации о каждой книге на экран
     for (int i = 0; i < numberOfBooks; ++i) {
         std::cout << "Информация о книге " << i + 1 << ":" << std::endl;
         books[i].displayBook();
