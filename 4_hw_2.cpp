1cf38572a79340267061334a927568f02644d7a9
archi <162299395+lulmealearn@users.noreply.github.com>
2024-10-25 18:15:55 +0300

    Update 4_hw_2.cpp

--- 4_hw_2.cpp
+++ 4_hw_2.cpp
@@ -1,103 +1,105 @@
 #include <iostream>
-#include <numeric>
-using namespace std;
+#define N 128
 
-int find(int* array, int value, int lenth){
-int counter = 0;
-cout<<"Поиск элемента в массиве типа int:\n";
-for(int i = 0; i<lenth; i++){
-  if (array[i]==value){
-    cout<<"Элемент есть в массиве типа int\n";
-    cout<<"Его индекс в массиве "<< i<<"\n";
-    counter = 1;
-    cout<<"\n";
-  }
-}
-if (counter==0){
-  cout<<"Такого элемента нет в массиве\n";
-  cout<<"\n";
-}
-}
-
-
-float find(float* array, float value, int lenth){
-int counter = 0;
-cout<<"Поиск элемента в массиве типа float\n";
-for(int i = 0; i<lenth; i++){
-  if (array[i]==value){
-    cout<<"Элемент есть в массиве типа float\n";
-    cout<<"Его индекс в массиве "<< i<<"\n";
-    counter = 1;
-    cout<<"\n";
-  }
-}
-if (counter==0){
-  cout<<"Такого элемента нет в массиве\n";
-  cout<<"\n";
-}
+void get (int n, int *m)
+{
+    int flag;
+    for(int i = 0; i<N; i++)
+    {
+        if(m[i]==n)
+        {
+            std::cout<<"found";
+            flag = 1;
+        }
+    }
+    if(flag != 1)
+    {
+        std::cout<<"not found";
+    }
 }
 
-char find(char* array, char value, int lenth){
-int counter = 0;
-cout<<"Поиск элемента в массиве типа char\n";
-for(int i = 0; i<lenth; i++){
-  if (array[i]==value){
-    cout<<"Элемент есть в массиве типа char\n";
-    cout<<"Его индекс в массиве "<< i<<"\n";
-    counter = 1;
-    cout<<"\n";
-  }
+void get (float n, float *m)
+{
+    int flag;
+    for(int i = 0; i<N; i++)
+    {
+        if(m[i]==n)
+        {
+            std::cout<<"found";
+            flag = 1;
+        }
+    }
+    if(flag != 1)
+    {
+        std::cout<<"not found";
+    }
 }
-if (counter==0){
-  cout<<"Такого элемента нет в массиве\n";
-  cout<<"\n";
+
+void get (char n, char *m)
+{
+    int flag;
+    for(int i = 0; i<N; i++)
+    {
+        if(m[i]==n)
+        {
+            std::cout<<"found";
+            flag = 1;
+        }
+    }
+    if(flag != 1)
+    {
+        std::cout<<"not found";
+    }
 }
-}
 
+main()
+{   
+    std::cout<<"Write type of data"<<'\n';
+    std::string str;
+    std::cin>>str;
+    std::cout<<"Write "<<N<<" elemnets of massive"<<'\n';
+    // поиск в массиве int
+    if (str == "int")
+    {  
+        int mas[N];
+        for (int i = 0; i<N;i++)
+        {
+            std::cin>>mas[i];
+        }
+        int *m = mas;
+        int n;
+        std::cin>>n;
+        get(n, m);
+    }
+    //поиск в массиве float
+    else if (str == "float")
+    {   
+        float mas[N];
+        for (int i = 0; i<N;i++)
+        {
+            std::cin>>mas[i];
+        }
+        float *m = mas;
+        float n;
+        std::cin>>n;
+        get(n, m);
+    }
 
-int main() {
-int size_integers, size_floating_point, size_character;
-int find_integers;
-float find_floating_point;
-char find_character;
-
-cout<<"Введите размер массива типа int\n";
-cin>> size_integers;
-cout<<"Введите массив типа int\n";
-int array_integers[size_integers];
-
-for(int i = 0; i < size_integers; i++){
-  cin>>array_integers[i];
-}
-cout<<"Введите элемент типа int, который необходимо найти\n";
-cin>>find_integers;
-
-
-cout<<"Введите размер массива типа float\n";
-cin>> size_floating_point;
-cout<<"Введите массив типа float\n";
-float array_floating_point[size_floating_point];
-
-for(int i = 0; i < size_floating_point; i++){
-  cin>>array_floating_point[i];
-}
-cout<<"Введите элемент типа float, который необходимо найти\n";
-cin>>find_floating_point;
-
-
-cout<<"Введите размер массива типа char\n";
-cin>> size_character;
-cout<<"Введите массив типа char\n";
-char array_character[size_character];
-
-for(int i = 0; i < size_character; i++){
-  cin>>array_character[i];
-}
-cout<<"Введите элемент типа char, который необходимо найти\n";
-cin>>find_character;
-
-find(array_integers, find_integers, size_integers);
-find(array_floating_point, find_floating_point, size_floating_point);
-find(array_character, find_character, size_character);
-
-}
+    else if (str == "char")
+    {   
+        char mas[N];
+        for (int i = 0; i<N;i++)
+        {
+            std::cin>>mas[i];
+        }
+        char *m = mas;
+        char n;
+        std::cin>>n;
+        get (n, m);
+    }
+    else
+    {
+        std::cout<<"Wrong input";
+    }
+    
+    
